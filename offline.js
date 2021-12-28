fde82df84e4ac7656374044318e1f310d95971fc
Izeple <izeple@hotmail.co.th>
2021-12-28 10:08:16 +0700

    Initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1640660789,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/rain-sheet0.png",
+		"images/face-sheet0.png",
+		"images/username_hi.png",
+		"images/endc_name.png",
+		"images/image-sheet0.png",
+		"images/ghost_1f47b-sheet0.png",
+		"images/ghost_1f47b-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
